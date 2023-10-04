bc52edc47d89249652806f0516d756f8138e0884
Josue Solorzano <41581607+blacknet101@users.noreply.github.com>
2023-10-03 19:22:11 -0700

    Summary

---
+++ Python Crash Course/Chapter 4 Working with Lists/LoopingLists.py
@@ -0,0 +1,26 @@
+techs = ['andy', 'josh', 'nick', 'ricky']
+# for tech in techs:
+#    print(tech.title())
+    
+for tech in techs:
+    print(f"{tech.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick {tech.title()}!\n")
+
+print("Thank you everyone! The best IT Team ever!\n")
+
+# 4-1. Pizzas.
+topic = "4-1. Pizzas.\n"
+print(topic)
+fav_pizzas = ['pepperoni', 'cheese', 'hawaiian']
+print("Favorite Pizzas:")
+for pizza in fav_pizzas:
+    print(f"I like {pizza.title()} pizza.")
+    
+# 4-2. Animals.
+topic = "\n4-2. animals.\n"
+print(topic)
+animals = ['dog', 'cat', 'wolves']
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet!")
+
+print("Any of these animals would make a great pet!")
